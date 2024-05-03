69bf72ec191de8e859947b50ad231ea67d6ceea1
Aovi02 <aovidiucocean02@gmail.com>
2024-05-03 11:11:06 +0200

    Clase Seleccion implementada

--- minitienda/WEB-INF/classes/minitienda/Seleccion.java
+++ minitienda/WEB-INF/classes/minitienda/Seleccion.java
@@ -1,5 +1,27 @@
 package minitienda;
 
 public class Seleccion {
-    
+    private CD cd;
+    private Integer cantidad;
+
+    public Seleccion(CD cd, Integer cantidad){
+        this.cd = cd;
+        this.cantidad = cantidad;
+    }
+
+    public void setCd(CD cd){
+        this.cd = cd;
+    }
+
+    public void setCantidad(Integer cantidad){
+        this.cantidad = cantidad;
+    }
+
+    public CD getCd(){
+        return this.cd;
+    }
+
+    public Integer getCantidad(){
+        return this.cantidad;
+    }
 }
